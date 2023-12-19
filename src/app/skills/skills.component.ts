dd480db514dd818936af9d09b07323ea0a82b513
drbenn <bennett.daniel@gmail.com>
2023-12-18 22:05:28 -0500

    tech tiles with image and placeholder animation in

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,58 +10,97 @@ export class SkillsComponent implements OnInit {
   protected sectionInView: boolean = false;
 
 
-  tech: {name:string, imgUrl: string}[] = [
+  tech: {name:string, imgUrl: string, enterAnimClass?: string, startPosClass: string, endPosClass: string}[] = [
     {
       name: 'Ang',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/angular.png',
+      enterAnimClass: 'skill-anim-angular',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'JS',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/js.png',
+      enterAnimClass: 'skill-anim-js',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'TS',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/ts.png',
+      enterAnimClass: 'skill-anim-ts',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'Node',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/node.png',
+      enterAnimClass: 'skill-anim-node',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'React',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/react.png',
+      enterAnimClass: 'skill-anim-react',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'MySQL',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/sql2.png',
+      enterAnimClass: 'skill-anim-sql',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'Git',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/git.png',
+      enterAnimClass: 'skill-anim-git',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'VSC',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/vsc.png',
+      enterAnimClass: 'skill-anim-vsc',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'JIRA',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/jira.png',
+      enterAnimClass: 'skill-anim-jira',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'HTML',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/html.png',
+      enterAnimClass: 'skill-anim-html',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'SASS',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/sass.png',
+      enterAnimClass: 'skill-anim-sass',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'Docker',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/docker.png',
+      enterAnimClass: 'skill-anim-docker',
+      startPosClass: '',
+      endPosClass: '',
     },
     {
       name: 'Postgres',
-      imgUrl: '',
+      imgUrl: '../assets/img/tech/postgres.png',
+      enterAnimClass: 'skill-anim-postgres',
+      startPosClass: '',
+      endPosClass: '',
     }
   ]
 
