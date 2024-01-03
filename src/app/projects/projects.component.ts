845919b4738a2488fff7cc2222a2f4373a14d720
drbenn <bennett.daniel@gmail.com>
2024-01-02 20:19:39 -0500

    portfolio card image cover

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,7 +9,8 @@ import { take } from 'rxjs';
 })
 export class ProjectsComponent implements OnInit {
   projects: any;
-  displayModal!: boolean;;
+  // displayModal!: boolean;;
+  visibleProjectsSidebar!: boolean;
 
   constructor(
     private dataService: ProjectDataService
@@ -28,12 +29,37 @@ export class ProjectsComponent implements OnInit {
     
   }
 
-  protected showModalDialog(): void {
-    this.displayModal = true;
+  // protected showModalDialog(): void {
+  //   this.displayModal = true;
+  // };
+
+  // protected dialogClose(): void {
+  //   this.displayModal = false; 
+  // };
+
+  protected projectsSidebarClose(): void {
+    this.visibleProjectsSidebar = false; 
   };
 
-  protected dialogClose(): void {
-    this.displayModal = false; 
-  };
+  protected getInlineStyle(url: string): any {
+    return {
+      // backgroundColor: 'blue',
+      // color: 'green',
+      // background: `url("${url}") no-repeat center center fixed`,
+      // backgroundRepeat: 'no-repeat',
+      // backgroundSize: 'cover',
+      // backgroundPosition: 'center',
+      // height: '100%',
+      // width: '10rem',
+      // objectFit: 'cover'
+      // background-image: url("../../assets/img/projects/history/mtlt.png");
+      // background: url("../../assets/img/projects/history/mtlt.png"),
+      // backgroundSize: 'cover',
+      // height: '8rem',
+      // backgroundRepeat: 'no-repeat',
+      // backgroundPosition: 'center' 
+
+    }
+  }
 
 }
